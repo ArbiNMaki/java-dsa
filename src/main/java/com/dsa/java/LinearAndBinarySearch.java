01193065ee4601a45e087bf6c60dab2383d7ed3e
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-08-04 15:33:34 +0700

    Linear Search

--- src/main/java/com/dsa/java/LinearAndBinarySearch.java
+++ src/main/java/com/dsa/java/LinearAndBinarySearch.java
@@ -4,14 +4,25 @@ public class LinearAndBinarySearch {
 
     public static void main(String[] args) {
         int[] numbers = {5,8,13,21,34,55,89,144,233,377};
-        int target = 144;
+//        int target = 12; "Element Not Found"
+        int target = 21; // 3
 
         int result = linearSearch(numbers, target);
 
-        System.out.println("Element found at Index : " + result);
+        if (result != -1) {
+            System.out.println("Element found at Index : " + result);
+        } else  {
+            System.out.println("Element not found!");
+        }
     }
 
     public static int linearSearch(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == target) {
+                return i;
+            }
+        }
+
         return -1;
     }
 }
