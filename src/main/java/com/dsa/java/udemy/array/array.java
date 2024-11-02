b7b3dd1cb5a7906f0de1b611fd8feef0c2688562
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-11-02 11:53:35 +0700

    basic array

---
+++ src/main/java/com/dsa/java/udemy/array/array.java
@@ -0,0 +1,21 @@
+package com.dsa.java.udemy.array;
+
+import java.util.Arrays;
+
+public class array {
+
+    public static void main(String[] args) {
+        // This array will store integer type element
+        int[] ar= { 1, 2, 3, 4, 5 };
+
+        // This array will store char type element
+        char[] arr = { 'a', 'b', 'c', 'd', 'e' };
+
+        // This array will store float type element
+        float[] arrr= { 1.4f, 2.0f, 24f, 5.0f, 0.0f };
+
+        System.out.println(Arrays.toString(ar));
+        System.out.println(arr);
+        System.out.println(Arrays.toString(arrr));
+    }
+}
