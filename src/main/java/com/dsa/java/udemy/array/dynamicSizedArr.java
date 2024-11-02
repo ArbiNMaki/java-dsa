ebd3c24637951c8eda27ecd0b1ec2bccddfc4c6d
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-11-02 11:57:54 +0700

    Fixed Sized and Dynamic Sized

---
+++ src/main/java/com/dsa/java/udemy/array/dynamicSizedArr.java
@@ -0,0 +1,11 @@
+package com.dsa.java.udemy.array;
+
+import java.util.ArrayList;
+
+public class dynamicSizedArr {
+
+    public static void main(String[] args) {
+        // Dynamic Integer Array
+        ArrayList<Integer> arr = new ArrayList<>();
+    }
+}
---
+++ src/main/java/com/dsa/java/udemy/array/fixedSizedArr.java
@@ -0,0 +1,14 @@
+package com.dsa.java.udemy.array;
+
+import java.util.Arrays;
+
+public class fixedSizedArr {
+
+    public static void main(String[] args) {
+        // Fixed sized array examples
+        int[] arr1 = new int [5];
+
+        // Another way (Array creation and initialization both)
+        int[] arr2 = {1, 2, 3, 4, 5};
+    }
+}
